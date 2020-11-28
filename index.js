241d78b0ae8acbcfbf5df9ac0fc23c81ac92ffeb
Oleg Lihogub <oligli@yandex.ru>
2020-11-28 21:42:14 +0300

    changed

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-console.log(`mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPASS}@\
-${process.env.MONGODBSERVER}:/${process.env.MONGODBNAME}?retryWrites=true&w=majority`);
-
-
 mongoose.connect(`mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPASS}@\
 ${process.env.MONGODBSERVER}:/${process.env.MONGODBNAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err)=>{if (err) console.log(err)}
-    );
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err) => {
+        if (err) console.log(err);
+    }
+);
 
 const AchievementSchema = new mongoose.Schema({id: Number, title: String, desc: String}, {collection: 'achievement'});
 const AdvantageSchema = new mongoose.Schema({id: Number, title: String, desc: String}, {collection: 'advantage'});
 const CompetencySchema = new mongoose.Schema({id: Number, text: String}, {collection: 'competency'});
 const ExpertiseSchema = new mongoose.Schema({id: Number, text: String}, {collection: 'expertise'});
-const RateSchema = new mongoose.Schema({id: Number, title: String, price: Number, features:[String]}, {collection: 'rate'});
+const RateSchema = new mongoose.Schema({
+    id: Number,
+    title: String,
+    price: Number,
+    features: [String]
+}, {collection: 'rate'});
 const WorkerSchema = new mongoose.Schema({id: Number, name: String, job: String}, {collection: 'worker'});
 
 const Achievement = mongoose.model('Achievement', AchievementSchema);
@@ -30,34 +32,45 @@ const Worker = mongoose.model('Worker', WorkerSchema);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! It is REST API server.');
 });
 
 app.get('/get/achievement', (req, res) => {
-    Achievement.find({}, {_id: 0}).exec((err, items)=>{res.send(items)})
+    Achievement.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.get('/get/advantage', (req, res) => {
-    Advantage.find({}, {_id: 0}).exec((err, items)=>{res.send(items)})
+    Advantage.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.get('/get/competency', (req, res) => {
-    Competency.find({}, {_id: 0}).exec((err, items)=>{res.send(items)})
+    Competency.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.get('/get/expertise', (req, res) => {
-    Expertise.find({}, {_id: 0}).exec((err, items)=>{res.send(items)})
+    Expertise.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.get('/get/rate', (req, res) => {
-    console.log("rate req");
-    Rate.find({}, {_id: 0}).exec((err, items)=>{console.log(items); res.send(items);})
+    Rate.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.get('/get/worker', (req, res) => {
-    Worker.find({}, {_id: 0}).exec((err, items)=>{res.send(items)})
+    Worker.find({}, {_id: 0}).exec((err, items) => {
+        res.send(items);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
